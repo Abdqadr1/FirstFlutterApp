6213ae1002b8d6076fc80a80bfb7e2b598fa2f33
Abdqadr1 <abolarinwaquadri@gmail.com>
2021-10-11 06:51:36 +0800

    profile card

--- lib/main.dart
+++ lib/main.dart
@@ -3,29 +3,8 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    home: MyProfile(),
   ));
-}
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("My First Flutter"),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
-      ),
-      body: MyProfile(),
-      backgroundColor: Colors.black87,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('click'),
-        backgroundColor: Colors.red[600],
-      ),
-
-    );
-  }
 }
 
 class MyProfile extends StatefulWidget {
@@ -37,31 +16,38 @@ class _MyProfileState extends State<MyProfile> {
   int num = 0;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0,0.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Center(
-            child: CircleAvatar(
-              backgroundImage: AssetImage("assets/laptop.jpg"),
-              radius: 50.0,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My First Flutter"),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0,0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/laptop.jpg"),
+                radius: 50.0,
+              ),
             ),
-          ),
-          Divider(
-            height: 90.0,
+            Divider(
+              thickness: 5.0,
+              height: 90.0,
               color: Colors.grey[200],
-          ),
-          Text(
-              "Name",
-            style: TextStyle(
-              color: Colors.grey,
-              letterSpacing: 2.0,
-              fontFamily: "Sofia"
             ),
-          ),
-          SizedBox(height: 10.0,),
-          Text(
+            Text(
+              "Name",
+              style: TextStyle(
+                  color: Colors.grey,
+                  letterSpacing: 2.0,
+                  fontFamily: "Sofia"
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
               "Abolarinwa Quadri",
               style: TextStyle(
                 color: Colors.yellowAccent,
@@ -69,46 +55,48 @@ class _MyProfileState extends State<MyProfile> {
                 letterSpacing: 2.0,
                 fontWeight: FontWeight.bold,
               ),
-          ),
-          SizedBox(height: 20.0),
-          Row(
-            children: <Widget>[
-              IconButton(
-                onPressed: () {
-                  setState(() {
-                    num++;
-                  });
-                },
-                icon: Icon(
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Icon(
                   Icons.mail_outline,
                   size: 30.0,
                   color: Colors.white12,
                 ),
-              ),
-              SizedBox(width: 7.0,),
-              Text(
-                'abolarinwaquadri@gmail.com',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 20.0,
-                  letterSpacing: 1.0,
-                ),
-              )
-            ],
-          ),
-          SizedBox(height: 40.0),
-          Center(
-            child: Text(
-              "You clicked $num times",
+                SizedBox(width: 7.0,),
+                Text(
+                  'abolarinwaquadri@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 40.0),
+            Center(
+              child: Text(
+                "You clicked $num times",
                 style: TextStyle(
                   color: Colors.redAccent,
-                  fontSize: 18.0,
+                  fontSize: 20.0,
                   fontWeight: FontWeight.bold,
                 ),
-            ),
-          )
-        ],
+              ),
+            )
+          ],
+        ),
       ),
+      backgroundColor: Colors.black87,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+      ),
+
     );
   }
 }
